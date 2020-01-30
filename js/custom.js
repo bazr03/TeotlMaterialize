620dfe9e3a98a456bd1302fb032669d79495a946
Ivan Bazaldua <icesarbr@gmail.com>
2020-01-30 17:36:10 -0600

    Creada branch homePage, trabajar en seccion de nuestro equipo

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
   //   var elems = document.querySelectorAll(".sidenav");
   //   var instances = M.Sidenav.init(elems, {});
 
+  // sideNav
   const sidenav = document.querySelector(".sidenav");
   M.Sidenav.init(sidenav, {});
 
+  // Parallac
+  const parallax = document.querySelectorAll(".parallax");
+  M.Parallax.init(parallax, {});
   // Slider
-  const slider = document.querySelector(".slider");
-  M.Slider.init(slider, {
-    indicators: false,
-    height: 500,
-    transition: 500,
-    interval: 6000
-  });
+  // const slider = document.querySelector(".slider");
+  // M.Slider.init(slider, {
+  //   indicators: false,
+  //   height: 500,
+  //   transition: 500,
+  //   interval: 6000
+  // });
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.slider');
-//     var instances = M.Slider.init(elems, options);
-//   });
