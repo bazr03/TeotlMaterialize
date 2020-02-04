47909b2e66aef7781a64c21f24348dd5df9a336e
Ivan Bazaldua <icesarbr@gmail.com>
2020-02-04 17:34:52 -0600

    Seccion nuestro equipo casi finalizada, falta mejorar aparienica

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   //   var elems = document.querySelectorAll(".sidenav");
   //   var instances = M.Sidenav.init(elems, {});
 
+  const currentYear = document.querySelector("#currentYear");
+  const year = new Date().getFullYear();
+  currentYear.innerHTML = year;
+
   // sideNav
   const sidenav = document.querySelector(".sidenav");
   M.Sidenav.init(sidenav, {});
@@ -9,6 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
   // Parallac
   const parallax = document.querySelectorAll(".parallax");
   M.Parallax.init(parallax, {});
+
+  // Carousel
+  const carousel = document.querySelectorAll(".carousel");
+  M.Carousel.init(carousel, {
+    duration: 200,
+    padding: 20,
+    indicators: true,
+    shift: 3
+  });
   // Slider
   // const slider = document.querySelector(".slider");
   // M.Slider.init(slider, {
